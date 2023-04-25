0cacd5cffdfce1b47afcd193e8b13c5000b422fd
bellgod00 <bellgod@curocom.com>
2023-04-25 15:36:01 +0900

    Test Commit

--- Example/ex01/main.java
+++ Example/ex01/main.java
@@ -2,4 +2,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hellow World!!");
     }
+
+    public static void Test(){
+        System.out.println("Git Test!!");
+    }
 }
